80c53ea3fdaf08d4c6e44797d141ac56d6bd6a1f
Radek <rad.bialkowski@gmail.com>
2023-07-25 19:19:56 +0200

    aggregate skills from offers and persist in the database

--- src/main/java/com/myapp/jobscraper/TechController.java
+++ src/main/java/com/myapp/jobscraper/TechController.java
@@ -7,6 +7,8 @@ import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,14 +29,41 @@ public class TechController {
 //        techStack.setAppearances(5);
 //        techRepository.save(techStack);
 
+        List<String> urls = new ArrayList<>();
+
         String job1 = "https://nofluffjobs.com/pl/job/senior-fullstack-developer-java-kotlin-networkedassets-wroclaw";
-        String html = getHtml(job1);
+        String job2 = "https://nofluffjobs.com/pl/job/fullstack-developer-react-java-emagine-warszawa-1";
+        urls.add(job1);
+        urls.add(job2);
 
-        getData(html);
+        List<TechStack> techStacks = new ArrayList<>();
 
-    }
 
-    private void getData(String html) {
+        for (String url : urls) {
+        String html = getHtml(url);
+        List<String> skills = getData(html);
+
+        skills.forEach(skill -> {
+           TechStack techStackToAdd = techStacks.stream()
+                    .filter(techStack -> techStack.getName().equals(skill))
+                    .findFirst()
+                    .orElseGet(() -> {
+                        TechStack newtechStack = new TechStack();
+                        newtechStack.setName(skill);
+                        return newtechStack;
+                    });
+            techStackToAdd.setAppearances(techStackToAdd.getAppearances() + 1);
+            techStacks.add(techStackToAdd);
+        });
+
+        techRepository.saveAll(techStacks);
+
+    }}
+
+    private List<String> getData(String html) {
+
+        List<String> skills = new ArrayList<>();
+
         String regexSection = "(?<=Obowiązkowe)[\\s\\S]*?(?=posting-nice-to-have)";
         String regexSkill = "class=\"ng-star-inserted\">([^<]+)";
 
@@ -48,9 +77,10 @@ public class TechController {
 
             while (matcherSkill.find()) {
                 String skill = matcherSkill.group(1);
-                System.out.println(skill);
+                skills.add(skill.trim());
             }
         }
+        return skills;
     }
 
     private String getHtml(String url) throws IOException {
