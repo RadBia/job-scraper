51b83f082eb8d69eca7c80f6300c6d7941f71567
Radek <rad.bialkowski@gmail.com>
2023-07-25 13:39:07 +0200

    Initial commit

---
+++ src/main/java/com/myapp/jobscraper/JobScraperApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.jobscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobScraperApplication.class, args);
+    }
+
+}
